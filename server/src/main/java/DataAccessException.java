1148ef79255ca59889755032b578ebe2dcbd8cc1
Carter Duncan <crduncan1000@gmail.com>
2024-02-26 14:42:31 -0700

    Installed starter code and added dependencies;

---
+++ server/src/main/java/DataAccessException.java
@@ -0,0 +1,11 @@
+import chess.*;
+package dataAccess;
+
+/**
+ * Indicates there was an error connecting to the database
+ */
+public class DataAccessException extends Exception{
+    public DataAccessException(String message) {
+        super(message);
+    }
+}
